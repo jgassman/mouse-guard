f824c55cc868433cdce9023c55fe748b229acd57
jwilson1293 <jthatcher1293@gmail.com>
2018-12-12 15:55:51 -0600

    Sort and count successes

--- app/controllers/dice.js
+++ app/controllers/dice.js
@@ -8,13 +8,20 @@ export default Controller.extend({
   results: computed(function() {
     return [];
   }),
+  resultsSorting: Object.freeze(['value']),
+  sortedResults: computed.sort('results', 'resultsSorting'),
+  successes: computed('results.[]', function() {
+    return this.results.filter(result => {
+      return result.value >= 4;
+    }).length
+  }),
 
   actions: {
     roll: function(){
       this.set('results', [])
       for(let i=0; i<this.quantity; ++i) {
         let value = Math.floor(Math.random() * (this.MAX - this.MIN) + this.MIN);
-        this.results.pushObject(value);
+        this.results.pushObject({'value': value});
       }
     }
   }
